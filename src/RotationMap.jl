11b0f548fb0e747af462036f362237edc3684ba4
Kenta Sato <bicycle1885@gmail.com>
2024-05-22 03:07:15 +0200

    add simple implementation

--- src/RotationMap.jl
+++ src/RotationMap.jl
@@ -1,5 +1,70 @@
 module RotationMap
 
-# Write your package code here.
+using ChainRulesCore: ChainRulesCore, unthunk
+using LinearAlgebra: svd, det
+using NNlib: batched_mul, batched_transpose
 
+checkdimension(A::AbstractArray) =
+    size(A, 1) == 9 || throw(DimensionMismatch("The input array should have 9 rows"))
+
+function rotation(A::AbstractMatrix)
+    checkdimension(A)
+    A = reshape(A, 3, 3, :)
+    U, _, V = batchedsvd(A)
+    ν = batchedsigndet(A)
+    # U * (-νI) * V'
+    U .*= reshape(ν, 1, 1, :)
+    batched_mul(U, batched_transpose(V))
 end
+
+function rotation(A::AbstractArray)
+    checkdimension(A)
+    rotation(reshape(A, 9, :))
+end
+
+function ChainRulesCore.rrule(::typeof(rotation), A::AbstractMatrix)
+    checkdimension(A)
+    A = reshape(A, 3, 3, :)
+    U, s, V = batchedsvd(A)
+    ν = batchedsigndet(A)
+    function rotation_pullback(R̄)
+        R̄ = unthunk(R̄)
+        # ∂A = U * (νG .* (K - K')) * V'
+        # where K = U'R̄*V and
+        #       G[i,j] = i == j ? 0 : inv(s[i] + s[j])
+        νG = zeros(3, 3, size(A, 3))
+        νG[1,2,:] .= νG[2,1,:] .= inv.(s[1,:] .+ s[2,:])
+        νG[1,3,:] .= νG[3,1,:] .= inv.(s[1,:] .+ s[3,:])
+        νG[2,3,:] .= νG[3,2,:] .= inv.(s[2,:] .+ s[3,:])
+        νG .*= reshape(ν, 1, 1, :)
+        K = batched_mul(batched_mul(batched_transpose(U), R̄), V)
+        ∂A = batched_mul(batched_mul(U, νG .* (K .- batched_transpose(K))), batched_transpose(V))
+        ChainRulesCore.NoTangent(), reshape(∂A, 9, :)
+    end
+    # U * (-νI) * V'
+    U .*= reshape(ν, 1, 1, :)
+    batched_mul(U, batched_transpose(V)), rotation_pullback
+end
+
+ChainRulesCore.rrule(::typeof(rotation), A::AbstractArray) = ChainRulesCore.rrule(rotation, A)
+
+# fallback for development
+function batchedsvd(A::AbstractArray{<: Union{Float32, Float64}, 3})
+    U = similar(A)
+    s = similar(A, 3, size(A, 3))
+    V = similar(A)
+    for k in axes(A, 3)
+        U[:,:,k], s[:,k], V[:,:,k] = svd(A[:,:,k])
+    end
+    U, s, V
+end
+
+function batchedsigndet(A::AbstractArray{<: Union{Float32, Float64}, 3})
+    d = similar(A, size(A, 3))
+    for k in axes(A, 3)
+        d[k] = ifelse(det(A[:,:,k]) > 0, 1, -1)
+    end
+    d
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
-using RotationMap
+using RotationMap: rotation
+using LinearAlgebra: det, I
+using ChainRulesTestUtils: test_rrule
 using Test
 
+isrotation(R) = det(R) ≈ 1 && R'R ≈ I
+
 @testset "RotationMap.jl" begin
-    # Write your tests here.
+    for T in [Float32, Float64]
+        A = randn(T, 9, 10)
+        R = rotation(A)
+        @test size(R) == (3, 3, 10)
+        @test eltype(R) == eltype(A)
+        @test all(isrotation(R[:,:,k]) for k in axes(R, 3))
+        # slightly relax the relative tolerance for Float32
+        test_rrule(rotation, A, rtol = √eps(T))
+    end
 end
